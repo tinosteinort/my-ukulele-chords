ca43610a8a35d99dd6b688114056840cfde0f566
Tino Steinort <tinosteinort@live.de>
2021-01-28 17:42:26 +0100

    add filter

--- site/js/main.js
+++ site/js/main.js
@@ -2,15 +2,40 @@ import { chords } from './modules/chords.js'
 
 require('../css/main.css')
 
-function main() {
-    const chordsDiv = document.getElementsByClassName('chords')[0];
-    chords.forEach(chord => chordsDiv.innerHTML += renderChordDiv(chord));
+function chordFilterInput() {
+    return document.getElementsByClassName('chord-filter')[0];
+}
+
+function chordsDiv() {
+    return document.getElementsByClassName('chords')[0];
+}
+
+function onFilterChange(e) {
+    renderChords(e.target.value);
+}
+function onFocusSelectText() {
+    chordFilterInput().select();
+}
+
+function renderChords(filterValue) {
+    const div = chordsDiv();
+    div.innerHTML = '';
+    chords
+        .filter(chord => filterValue == undefined || chord.name.toLowerCase().startsWith(filterValue.toLowerCase()))
+        .forEach(chord => div.innerHTML += renderChordDiv(chord));
 }
 
 function renderChordDiv(chord) {
     return `<div class=\"chord pure-u-1 pure-u-sm-1-2 pure-u-md-1-3\">
         <img src=\"${chord.file}\" alt=\"Chord ${chord.name}\">
     </div>`;
+}
+
+
+function main() {
+    chordFilterInput().addEventListener('input', onFilterChange)
+    chordFilterInput().addEventListener('focus', onFocusSelectText)
+    renderChords()
 }
 
 window.addEventListener('load', main);
