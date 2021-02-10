aad543f2a6be186a22207adc5e25c6a7160cab64
Tino Steinort <tinosteinort@live.de>
2021-02-10 21:06:09 +0100

    prepare training

--- site/js/modules/training/training.js
+++ site/js/modules/training/training.js
@@ -1,3 +1,4 @@
+import { chords } from './../chords.js'
 import html from './training.html';
 
 export const createTraining = function() {
@@ -5,9 +6,40 @@ export const createTraining = function() {
     const training = document.createElement('div');
     training.innerHTML = html;
 
-    const trainButton = training.querySelector('#train');
+    const startTrainingButton = training.querySelector('#train');
+    const stopTrainingButton = training.querySelector('#stop');
+    const chordInputField = training.querySelector('.chord-input');
+    const validationInfo = training.querySelector('.validation-info');
+
+    function chordsAreValid(chordsToTrain) {
+        const validChordNames = chords.map(c => c.name.toLowerCase())
+        return chordsToTrain
+                .map(chordToCheck => validChordNames.includes(chordToCheck))
+                .includes(false) == false;
+    }
+
+    function startTraining() {
+        const chordInputText = chordInputField.value.trim();
+        if (chordInputText === "") {
+            return;
+        }
+
+        const chordNames = chordInputText.split(',')
+            .map(chordName => chordName.toLowerCase());
+
+        if (chordsAreValid(chordNames)) {
+            validationInfo.classList.add('hidden');
+        }
+        else {
+            validationInfo.classList.remove('hidden');
+        }
+    }
+    function stopTraining() {
+
+    }
     
-    trainButton.addEventListener('click',()=> console.log('train'));
+    startTrainingButton.addEventListener('click', startTraining);
+    stopTrainingButton.addEventListener('click', stopTraining);
 
     return training;
 }
